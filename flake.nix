6e90b4f31b7ba996427cb602b79e44509dbbac3f
trevorswan11 <tcs94@case.edu>
2026-02-01 18:39:54 -0500

    add flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { nixpkgs, utils, rust-overlay, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustToolchain = pkgs.rust-bin.stable.latest.default;
+      in
+      {
+        devShells.default = pkgs.mkShellNoCC {
+          buildInputs = with pkgs; [
+            rustToolchain
+            rust-analyzer
+            git
+          ];
+        };
+      }
+    );
+}
